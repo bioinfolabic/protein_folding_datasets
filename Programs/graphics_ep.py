a146fe50a468a8d729921ec61113b3d52bb97138
LABIC <bioinfolabic@gmail.com>
2019-04-22 09:41:57 -0300

    Update graphics_ep.py

--- Programs/graphics_ep.py
+++ Programs/graphics_ep.py
@@ -4,84 +4,70 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # numero de arquivos a serem lidos
-n_arquivos = 1000
-n_dados = 1000
-max_step = 3000000
+n_arquivos1 = 1000
+n_dados1 = 1000
+max_step1 = 3000000
+step1 = 3000
+id = 160
 
 # criando o vetor da energia exponencial
 vetor_Ep1 = [] # um vetor dinâmico para a energia exponencial de um arquivo
-vetor_Ep2 = []
-vetor_Ep3 = []
 
 vetor_all_Ep1 = []
-vetor_all_Ep2 = []
-vetor_all_Ep3 = []
 
 vetor_all_Ep1_media = [] # vetor com a média de todos os vetores
-vetor_all_Ep2_media = []
-vetor_all_Ep3_media = []
 
-vetor_step = []
+vetor_all_Ep1_media_normalizado = []
 
+vetor_step1 = []
 
-# pegando os valores de cada Step
-for i in xrange(0, max_step):
-        if i%3000==0 :
-                vetor_step.append(i)
+data = []
+
+# pegando os valores de cada step1
+vetor_step1.append(1)
+for i in xrange(2, max_step1):
+        if (i%step1==0):
+                vetor_step1.append(i)
 # pegando as enegias de todos os arquivos
-for i in xrange(1, n_arquivos+1):
-    print i
-    with open("/home/bruna/heatmap/13_fibonacci/ABBABBABABBAB_pathwaystep3000_"+str(i+1)+"_pathways.txt", "r") as input: ################################################ CHANGE HERE 13FIBO
+for i in xrange(1, n_arquivos1+1):
+    with open("/home/bruna/DM/Proteinas/13FIBO/pathways/ABBABBABABBAB_pathwaystep3000_"+str(i)+"_pathways.txt", "r") as input: ################################################ CHANGE HERE
         vetor_Ep1 = [] # limpando o vetor
         for line in input: # passando de linha em linha procurando os dados no arquivo
-            if 'Potential' in line:
-                vetor_Ep1.append(float(line.replace("Potential Energy = ","")))
-    with open("/home/bruna/heatmap/2GB1_56/pathways56_"+str(i)+".txt", "r") as input: # ################################################################################# CHANGE HERE 2GB1
-        vetor_Ep2 = [] # limpando o vetor
-        for line in input: # passando de linha em linha procurando os dados no arquivo
-            if 'Potential' in line:
-                vetor_Ep2.append(float(line.replace("Potential Energy = ","")))
-    with open("/home/bruna/heatmap/1PCY_99/pathways99_"+str(i)+".txt", "r") as input: # ################################################################################# CHANGE HERE 1PLC
-        vetor_Ep3 = [] # limpando o vetor
-        for line in input: # passando de linha em linha procurando os dados no arquivo
-            if 'Potential' in line:
-                vetor_Ep3.append(float(line.replace("Potential Energy = ","")))
+                if 'Potential' in line:
+                        vetor_Ep1.append(float(line.replace("Potential Energy = ","")))
         vetor_all_Ep1.append(vetor_Ep1)
-        vetor_all_Ep2.append(vetor_Ep2)
-        vetor_all_Ep3.append(vetor_Ep3)
 # fazendo a média de todos os vetores
-for k in xrange(0, n_dados):
+for k in xrange(0, n_dados1):
         soma1 = 0
-        soma2 = 0
-        soma3 = 0
-        for j in xrange(0, n_arquivos):
+        for j in xrange(0, n_arquivos1):
                 soma1 = soma1 + vetor_all_Ep1[j][k] # fazendo a média do primeiro dado de cada arquivo, então temos que passar por todos os arquivos rapidamente
-                soma2 = soma2 + vetor_all_Ep2[j][k]
-                soma3 = soma3 + vetor_all_Ep3[j][k]
-        soma1 = soma1/n_arquivos # assim temos a média dos dados dos n arquivos
-        soma2 = soma2/n_arquivos
-        soma3 = soma3/n_arquivos
+        soma1 = soma1/n_arquivos1 # assim temos a média dos dados dos n arquivos
         vetor_all_Ep1_media.append(soma1) # vetor com a média de todos os vetores
-        vetor_all_Ep2_media.append(soma2)
-        vetor_all_Ep3_media.append(soma3)
-# print vetor_all_media
-print "fim! :D"
-print vetor_all_Ep1_media[999]
-print vetor_all_Ep2_media[999]
-print vetor_all_Ep3_media[999]
+max = vetor_all_Ep1_media[990]
+print 'max = ' + str(max)
+for k in xrange(0, n_dados1):
+        vetor_all_Ep1_media_normalizado.append(vetor_all_Ep1_media[k]/(-1*max)) # divide por -1*max
+
+# calculando o desvio padrão do último valor da energia potencial
+'''for i in xrange (0, n_arquivos1):
+        data.append(vetor_all_Ep1[i][999])
+v = np.var(data)
+d = np.sqrt(v)
+print "Media da enerdia potencial final = " + str(vetor_all_Ep1_media[999])
+print "Desvio padrão = " + str(d)
+print "fim! :D"'''
 # agora fazendo o gráfico dos dados recolhidos
-plt.plot(vetor_step, vetor_all_Ep1_media, label="13 fibonacci")
-plt.plot(vetor_step, vetor_all_Ep2_media, label="2GB1")
-plt.plot(vetor_step, vetor_all_Ep3_media, label="1PCY")
+plt.plot(vetor_step1, vetor_all_Ep1_media_normalizado) # tirei , label="5NAZ"
 # deixando em notação científica
 plt.ticklabel_format(axis='both', style='sci', scilimits=(0,0), useMathText=True)
 # escrevendo o título e o nome dos eixos em inglês
-plt.xlabel('Pathway step (1e6)', labelpad=10, fontsize=15)
-plt.ylabel('Average Ep (1e2)', labelpad=10, fontsize=15)
-plt.xticks(fontsize = 15)
-plt.yticks(fontsize = 15)
+plt.xlabel('Pathway step (x1e6)', labelpad=10, fontsize=25)
+plt.ylabel('Average Ep (x24.92)', labelpad=10, fontsize=25)
+plt.xticks(fontsize = 25)
+plt.yticks(fontsize = 25)
+plt.ylim(ymin=-1.05,ymax=0.05)
 plt.grid(True)
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0., fontsize = 15)
+#plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0., fontsize = 15)
 plt.rcParams['figure.figsize'] = (11,7)
-#plt.savefig('average_Ep.png', orientation='landscape', dpi=100, bbox_inches='tight')
+plt.savefig('/home/bruna/DM/graficos_ep/graphic_average_ep_13FIBO.png', orientation='landscape', dpi=100, bbox_inches='tight')
 plt.show()
